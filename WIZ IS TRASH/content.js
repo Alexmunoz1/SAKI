6a7b3fcf080e8c79d623828078ffb93f55e8b8f7
Alexmunoz1 <alejandro.munoz1@speeracademy.org>
2017-06-06 08:58:36 -0500

    v1.0.0.0 Changed colors
    I made the page change colors and I also made the background and button work so it could go to a different page. I linked the button to my YouTube and it changes the color of the text.

--- WIZ IS TRASH/content.js
+++ WIZ IS TRASH/content.js
@@ -1,4 +1,5 @@
 var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+  images[i].src = '  images[i].src = '' + images[i].width + '/' + images[i].height;
+' + images[i].width + '/' + images[i].height;
 }
